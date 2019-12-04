9c5951d6f6e712fa0301087c5171d9d9ed50bbb4
Johnson Zhong <zhsh@umich.edu>
2019-12-04 13:04:40 -0500

    Merge data files in directory

--- arm_pytorch_utilities/load_data.py
+++ arm_pytorch_utilities/load_data.py
@@ -1,5 +1,8 @@
 import torch
 import torch.utils.data
+import numpy as np
+import scipy.io
+import os
 
 
 class RandomNumberDataset(torch.utils.data.Dataset):
@@ -48,6 +51,7 @@ class SimpleDataset(torch.utils.data.Dataset):
 
 class IndexedDataset(SimpleDataset):
     """Same as before, but with last element as the index of the data point for using in Dataloaders"""
+
     def __getitem__(self, idx):
         return tuple(sequence[idx] for sequence in self.sequences) + (idx,)
 
@@ -76,3 +80,23 @@ def splitTrainValidationSets(dataset, validation_ratio=0.1):
     # consider giving a shuffle (with np.random.shuffle()) option to permute the data before viewing
     offset = int(len(dataset) * (1 - validation_ratio))
     return PartialViewDataset(dataset, 0, offset), PartialViewDataset(dataset, offset, len(dataset) - offset)
+
+
+def merge_data_in_dir(config, dir, out_filename, sort=True):
+    full_dir = os.path.join(config.DATA_DIR, dir)
+
+    files = os.listdir(full_dir)
+    if sort:
+        files = sorted(files)
+
+    data = None
+    for file in files:
+        full_filename = '{}/{}'.format(full_dir, file)
+        raw_data = scipy.io.loadmat(full_filename)
+        if data is None:
+            data = raw_data
+        else:
+            for key in raw_data.keys():
+                data[key] = np.row_stack((data[key], raw_data[key]))
+    merged_filename = '{}/{}.mat'.format(config.DATA_DIR, out_filename)
+    scipy.io.savemat(merged_filename, data)
