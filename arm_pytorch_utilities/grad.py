7fee27dbb57657ed567bf0d00c421b9298b98d45
Johnson Zhong <zhsh@umich.edu>
2019-12-31 00:27:01 -0700

    Add jacobian calculation

---
+++ arm_pytorch_utilities/grad.py
@@ -0,0 +1,22 @@
+import torch
+
+
+def jacobian(f, x):
+    """
+    Compute the jacobian of f evaluated at x. If f is a network or has parameters, freezing it
+    (set requires_grad = False) improves performance (especially on large networks)
+
+    From https://gist.github.com/sbarratt/37356c46ad1350d4c30aefbd488a4faa
+    :param f: f(x) -> y
+    :param x: 1 x nx or nx input to evaluate the jacobian at
+    :return: df/dx(x) ny x nx jacobian of f evaluated at x
+    """
+    if x.dim() < 2:
+        x = x.view(1, -1)
+    y = f(x)
+    noutputs = y.shape[1]
+    x = x.repeat(noutputs, 1)
+    x.requires_grad_(True)
+    y = f(x)
+    y.backward(torch.eye(noutputs))
+    return x.grad.data
---
+++ tests/test_grad.py
@@ -0,0 +1,41 @@
+import torch
+from arm_pytorch_utilities import grad
+
+
+def test_jacobian():
+    def f1(x):
+        return torch.cat((x ** 2, (2 * x[:, 0] + torch.log(x[:, 1])).view(-1, 1)), dim=1)
+
+    nx = 2
+    nout_f1 = nx + 1
+    x = torch.rand(nx)
+    j1 = grad.jacobian(f1, x)
+    j2 = grad.jacobian(f1, x.view(1, -1))
+    assert j1.shape == (nout_f1, nx)
+    assert torch.allclose(j1, j2)
+    assert torch.allclose(j1[:, 0], torch.tensor([2 * x[0], 0, 2.]))
+    assert torch.allclose(j1[:, 1], torch.tensor([0., 2 * x[1], 1. / x[1]]))
+
+    nx = 5
+    nout_f2 = 3
+    net = torch.nn.Linear(nx, nout_f2, bias=False)
+
+    def f2(x):
+        return net(x)
+
+    x = torch.randn(1, nx)
+    # test that it doesn't matter if other operations are ran on the network before calculating jacobian
+    f2(x)
+    j3 = grad.jacobian(f2, x)
+    assert j3.shape == (nout_f2, nx)
+    assert torch.allclose(j3, net.weight)
+
+    # can even have existing gradients calculated from some other backward branch
+    y = f2(x)
+    y.sum().backward()
+    j3 = grad.jacobian(f2, x)
+    assert torch.allclose(j3, net.weight)
+
+
+if __name__ == "__main__":
+    test_jacobian()
