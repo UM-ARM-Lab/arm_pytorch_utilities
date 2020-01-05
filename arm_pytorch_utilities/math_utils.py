74a33316830c17c63c7ef12aeb9dec985a18f4a0
Johnson Zhong <zhsh@umich.edu>
2020-01-04 23:29:52 -0700

    Add convenience function for defining two bounds

--- arm_pytorch_utilities/math_utils.py
+++ arm_pytorch_utilities/math_utils.py
@@ -7,6 +7,16 @@ def clip(a, min_val, max_val):
     return torch.max(torch.min(a, max_val), min_val)
 
 
+def get_bounds(u_min=None, u_max=None):
+    """Convenience for ensuring a bound if given is two sided"""
+    # make sure if any of them is specified, both are specified
+    if u_max is not None and u_min is None:
+        u_min = -u_max
+    if u_min is not None and u_max is None:
+        u_max = -u_min
+    return u_min, u_max
+
+
 def rotate_wrt_origin(xy, theta):
     return (xy[0] * math.cos(theta) + xy[1] * math.sin(theta),
             -xy[0] * math.sin(theta) + xy[1] * math.cos(theta))
