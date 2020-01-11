d06176390d3bffdb99f214d3c3bc0e471c742c65
Johnson Zhong <zhsh@umich.edu>
2020-01-11 14:57:34 -0500

    Hide implementation

--- arm_pytorch_utilities/array_utils.py
+++ arm_pytorch_utilities/array_utils.py
@@ -34,17 +34,17 @@ def extract_positive_weights(weights):
     """
     # assume floating point type means weights are soft
     if torch.is_floating_point(weights):
-        return use_soft_weights(weights)
-    return use_hard_weights(weights)
+        return _extract_soft_weights(weights)
+    return _extract_hard_weights(weights)
 
 
-def use_soft_weights(w):
+def _extract_soft_weights(w):
     neighbours = w > 0
     nw = w[neighbours]
     return neighbours, nw
 
 
-def use_hard_weights(w):
+def _extract_hard_weights(w):
     if torch.is_tensor(w):
         return w, torch.ones_like(w).double()
     return w, np.ones_like(w)
