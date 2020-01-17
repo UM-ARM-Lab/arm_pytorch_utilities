Create batch rotate wrt origin and fix non-batch rotation to actually rotate the vector
Used to be flipped before

arm_pytorch_utilities/math_utils.py
@@ -1,6 +1,7 @@
 import math
 
 import torch
+from arm_pytorch_utilities import linalg
 
 
 def clip(a, min_val, max_val):
@@ -27,8 +28,18 @@ def get_bounds(u_min=None, u_max=None):
 
 
 def rotate_wrt_origin(xy, theta):
-    return (xy[0] * math.cos(theta) + xy[1] * math.sin(theta),
-            -xy[0] * math.sin(theta) + xy[1] * math.cos(theta))
+    return (xy[0] * math.cos(theta) - xy[1] * math.sin(theta),
+            xy[0] * math.sin(theta) + xy[1] * math.cos(theta))
+
+
+def batch_rotate_wrt_origin(xy, theta):
+    # create rotation matrices
+    c = torch.cos(theta).view(-1, 1)
+    s = torch.sin(theta).view(-1, 1)
+    top = torch.cat((c, s), dim=1)
+    bot = torch.cat((-s, c), dim=1)
+    R = torch.cat((top.unsqueeze(2), bot.unsqueeze(2)), dim=2)
+    return linalg.batch_batch_product(xy, R)
 
 
 def angular_diff(a, b):

tests/test_math.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 from arm_pytorch_utilities import math_utils
 
@@ -6,9 +8,21 @@ def test_angle_normalize():
     N = 100
     theta = torch.randn(N) * 5
     t1 = math_utils.angle_normalize(theta)
-    assert torch.allclose(torch.cos(theta), torch.cos(t1),  atol=1e-6)
-    assert torch.allclose(torch.sin(theta), torch.sin(t1),  atol=1e-6)
+    assert torch.allclose(torch.cos(theta), torch.cos(t1), atol=1e-6)
+    assert torch.allclose(torch.sin(theta), torch.sin(t1), atol=1e-6)
+
+
+def test_batch_angle_rotate():
+    N = 100
+    xy = torch.tensor([1., 0]).repeat(N, 1)
+    theta = (torch.rand(N) - 0.5) * 2 * math.pi
+
+    xyr = math_utils.batch_rotate_wrt_origin(xy, theta)
+    for i in range(N):
+        r = math_utils.rotate_wrt_origin(xy[i], theta[i])
+        assert torch.allclose(torch.tensor(r), xyr[i])
 
 
 if __name__ == "__main__":
     test_angle_normalize()
+    test_batch_angle_rotate()