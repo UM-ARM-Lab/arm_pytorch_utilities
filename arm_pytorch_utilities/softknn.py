801da2035e50c60a54b806e77b272dbc1e8843c2
Johnson Zhong <zhsh@umich.edu>
2020-01-21 21:23:47 -0500

    Add SoftKNN

---
+++ arm_pytorch_utilities/softknn.py
@@ -0,0 +1,58 @@
+import torch
+
+
+class SoftKNN(torch.nn.Module):
+    """
+    Differentiable relaxation of the k-nearest neighbour operation.
+    For a N x n feature input, we output a N x N weight matrix such that the (i,j) element is the weight of the
+    jth data point of being one of the k nearest neighbours of the ith data point. Weights are non-negative and
+    their upper value range depends on initialization options.
+    """
+
+    def __init__(self, min_k=5, activation='linear', uniform_threshold=False, normalization=None):
+        """
+        :param min_k: Minimum number of neighbours to consider at each point; the behaviour depends on what activation
+        is used and whether a uniform threshold is chosen
+        :param activation: The function to apply to the shifted distances to get positive weights; currently there is
+        'linear' which is ReLU and otherwise we use sigmoid. We recover hard KNN by using the sign activation function
+        (1 for distance >= 0 and 0 otherwise).
+        :param uniform_threshold: Whether a single distance threshold should be chosen for all data points such that
+        at least min_k points is guaranteed to have positive weights for every data point
+        :param normalization: None or the p value for the L_p norm, use 1 for sum to 1, use 2 for norm to 1
+        """
+        super(SoftKNN, self).__init__()
+        self.min_neighbours = min_k
+        self.activation = activation
+        self.normalization = normalization
+        self.uniform_threshold = uniform_threshold
+
+    def forward(self, features):
+        # calculate the distances of every point to every other point in feature space
+        dists = torch.cdist(features, features)
+
+        # select a distance threshold such that any point beyond this is weighted 0
+        # basically the max of the kth smallest element inside each row
+        dd, ddi = torch.topk(dists, self.min_neighbours + 1, largest=False, sorted=False, dim=1)
+        threshold = dd[:, self.min_neighbours]
+
+        if self.uniform_threshold:
+            threshold = torch.max(threshold)
+            dd = (threshold - dists)
+        else:
+            dd = (threshold[:, None] - dists)
+
+        # avoid edge case of multiple elements at kth closest distance causing them to become 0
+        dd += 1e-10
+
+        # TODO add more activation options
+        # apply weighting function to the distances/nodes
+        if self.activation is 'linear':
+            weights = dd.clamp(min=0)
+        else:
+            weights = torch.sigmoid(dd * self.activation)
+
+        # normalization
+        if self.normalization is not None:
+            weights = torch.nn.functional.normalize(weights, dim=1, p=self.normalization)
+
+        return weights
---
+++ tests/test_softknn.py
@@ -0,0 +1,177 @@
+"""Verify that our soft KNN method passes through gradients and is able to learn parameters through it"""
+import logging
+
+import torch
+from arm_pytorch_utilities import load_data, rand
+from arm_pytorch_utilities import softknn
+from matplotlib import pyplot as plt
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s %(asctime)s %(pathname)s:%(lineno)d] %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+
+
+# generate ground truth transforms and operations
+# converts it to a transformed space
+class TestNet(torch.nn.Module):
+    def __init__(self, D_in, H):
+        super(TestNet, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H, bias=False)
+        self.knn = softknn.SoftKNN(min_k=20)
+
+    def forward(self, x, y):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        features = self.linear1(x)
+
+        weights = self.knn(features)
+        # TODO weighted least squares in x-y space rather than feature space using these weights
+        # propagate gradient backwards to transform parameters through these weights
+        # TODO remove: for now can just sum up xs inside the neighbourhood
+
+        output = torch.zeros_like(x)
+        for i, w in enumerate(weights):
+            # nw = w / torch.sum(w)
+            # output[i] = torch.matmul(nw, x)
+            # can drop out the terms that have 0 weight and seeing if it will affect differentiability
+            neighbours = torch.nonzero(w).view(-1)
+            nw = w[neighbours]
+            nw /= torch.sum(nw)
+            output[i] = torch.matmul(nw, x[neighbours])
+
+        return output
+
+
+def KNN(features, k):
+    # features = features.float()
+    dist_mat = torch.cdist(features, features)
+    # ith row are the k nearest neighbours of ith data point
+    dists, Idx = torch.topk(dist_mat, k, largest=False, sorted=False, dim=1)
+    return dists, Idx
+
+
+def test_softknn(debug=False):
+    # doesn't always converge in time for all random seed
+    seed = 318455
+    logger.info('random seed: %d', rand.seed(seed))
+
+    D_in = 3
+    D_out = 1
+
+    target_params = torch.rand(D_in, D_out).t()
+    # target_params = torch.tensor([[1, -1, 1]], dtype=torch.float )
+    target_tsf = torch.nn.Linear(D_in, D_out, bias=False)
+    target_tsf.weight.data = target_params
+    for param in target_tsf.parameters():
+        param.requires_grad = False
+
+    def produce_output(X):
+        # get the features
+        y = target_tsf(X)
+        # cluster in feature space
+        dists, Idx = KNN(y, 5)
+
+        # take the sum inside each neighbourhood
+        # TODO do a least square fit over X inside each neighbourhood
+        features2 = torch.zeros_like(X)
+        for i in range(dists.shape[0]):
+            # md = max(dists[i])
+            # d = md - dists[i]
+            # w = d / torch.norm(d)
+            features2[i] = torch.mean(X[Idx[i]], 0)
+            # features2[i] = torch.matmul(w, X[Idx[i]])
+
+        return features2
+
+    N = 400
+    ds = load_data.RandomNumberDataset(produce_output, num=400, input_dim=D_in)
+    train_set, validation_set = load_data.split_train_validation(ds)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=N, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(validation_set, batch_size=N, shuffle=False)
+
+    criterion = torch.nn.MSELoss(reduction='sum')
+
+    model = TestNet(D_in, D_out)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+
+    losses = []
+    vlosses = []
+    pdist = []
+    cosdist = []
+
+    def evaluateLoss(data):
+        # target
+        x, y = data
+        pred = model(x, y)
+
+        loss = criterion(pred, y)
+        return loss
+
+    def evaluateValidation():
+        with torch.no_grad():
+            loss = sum(evaluateLoss(data) for data in val_loader)
+            return loss / len(val_loader.dataset)
+
+    # model.linear1.weight.data = target_params.clone()
+    for epoch in range(200):
+        for i, data in enumerate(train_loader, 0):
+            optimizer.zero_grad()
+
+            loss = evaluateLoss(data)
+            loss.backward()
+            optimizer.step()
+
+            avg_loss = loss.item() / len(data[0])
+
+            losses.append(avg_loss)
+            vlosses.append(evaluateValidation())
+            pdist.append(torch.norm(model.linear1.weight.data - target_params))
+            cosdist.append(torch.nn.functional.cosine_similarity(model.linear1.weight.data, target_params))
+            if debug:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, avg_loss))
+
+    if debug:
+        print('Finished Training')
+        print('Target params: {}'.format(target_params))
+        print('Learned params:')
+        for param in model.parameters():
+            print(param)
+
+        print('validation total loss: {:.3f}'.format(evaluateValidation()))
+
+    model.linear1.weight.data = target_params.clone()
+    target_loss = evaluateValidation()
+
+    if debug:
+        print('validation total loss with target params: {:.3f}'.format(target_loss))
+
+        plt.plot(range(len(losses)), losses)
+        plt.plot(range(len(losses)), vlosses)
+        plt.plot(range(len(losses)), [target_loss] * len(losses), linestyle='--')
+        plt.legend(['training minibatch', 'whole validation', 'validation with target params'])
+        plt.xlabel('minibatch')
+        plt.ylabel('MSE loss')
+
+        plt.figure()
+        plt.plot(range(len(pdist)), pdist)
+        plt.xlabel('minibatch')
+        plt.ylabel('euclidean distance of model params from target')
+
+        plt.figure()
+        plt.plot(range(len(cosdist)), cosdist)
+        plt.xlabel('minibatch')
+        plt.ylabel('cosine similarity between model params and target')
+        plt.show()
+
+    # check that we're close to the actual KNN performance on validation set
+    last_few = 5
+    loss_tolerance = 0.02
+    assert sum(vlosses[-last_few:]) / last_few - target_loss < target_loss * loss_tolerance
+
+
+if __name__ == "__main__":
+    test_softknn(True)
