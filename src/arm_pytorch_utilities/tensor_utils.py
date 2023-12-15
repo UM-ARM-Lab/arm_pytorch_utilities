27939487ba84086c8b103860e25c846186b95d5e
Sheng Zhong <zhsh@umich.edu>
2023-12-15 13:55:47 -0500

    Generalize handle batch input to n dimensions

--- src/arm_pytorch_utilities/tensor_utils.py
+++ src/arm_pytorch_utilities/tensor_utils.py
@@ -29,35 +29,64 @@ def _interpret_batch_output(batch_dims, v):
     return v.reshape(*batch_dims)
 
 
-def handle_batch_input(func):
-    """
-    For func that expect 2D input, handle input that have more than 2 dimensions by flattening them temporarily
-    It tries to infer the batch dimensions from the first tensor-like input.
-    """
+def squeeze_n(v, n_squeeze):
+    for _ in range(n_squeeze):
+        v = v.squeeze(0)
+    return v
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        # assume inputs that are tensor-like have compatible shapes and is represented by the first argument
-        batch_dims = []
-        for arg in args:
-            if is_tensor_like(arg) and len(arg.shape) > 2:
-                batch_dims = arg.shape[:-1]  # last dimension is type dependent; all previous ones are batches
-                break
-        # no batches; just return normally
-        if not batch_dims:
-            return func(*args, **kwargs)
 
-        # reduce all batch dimensions down to the first one
-        args = [v.reshape(-1, v.shape[-1]) if (is_tensor_like(v) and len(v.shape) > 2) else v for v in args]
-        ret = func(*args, **kwargs)
-        # restore original batch dimensions; keep variable dimension (nx)
-        if type(ret) is tuple:
-            ret = [_interpret_batch_output(batch_dims, v) for v in ret]
-        else:
-            ret = _interpret_batch_output(batch_dims, ret)
-        return ret
+# from arm_pytorch_utilities, standalone since that package is not on pypi yet
+def handle_batch_input(n=2):
+    """Decorator for functions that expect n-dimensional input, handle input that have more than n dimensions by
+    flattening them temporarily and intelligently reshaping the output to match the input."""
 
-    return wrapper
+    def _handle_batch_input(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # assume inputs that are tensor-like have compatible shapes and is represented by the first argument
+            batch_dims = []
+            for arg in args:
+                if is_tensor_like(arg):
+                    if len(arg.shape) > n:
+                        # last dimension is type dependent; all previous ones are batches
+                        batch_dims = arg.shape[:-(n - 1)]
+                        break
+                    elif len(arg.shape) < n:
+                        n_batch_dims_to_add = n - len(arg.shape)
+                        batch_ones_to_add = [1] * n_batch_dims_to_add
+                        args = [v.view(*batch_ones_to_add, *v.shape) if is_tensor_like(v) else v for v in args]
+                        ret = func(*args, **kwargs)
+                        if isinstance(ret, tuple):
+                            ret = [squeeze_n(v, n_batch_dims_to_add) if is_tensor_like(v) else v for v in ret]
+                            return ret
+                        else:
+                            if is_tensor_like(ret):
+                                return squeeze_n(ret, n_batch_dims_to_add)
+                            else:
+                                return ret
+            # no batches; just return normally
+            if not batch_dims:
+                return func(*args, **kwargs)
+
+            # reduce all batch dimensions down to the first one
+            args = [v.view(-1, *v.shape[-(n - 1):]) if (is_tensor_like(v) and len(v.shape) > 2) else v for v in args]
+            ret = func(*args, **kwargs)
+            # restore original batch dimensions; keep variable dimension (nx)
+            if type(ret) is tuple:
+                ret = [v if (not is_tensor_like(v) or len(v.shape) == 0) else (
+                    v.view(*batch_dims, *v.shape[-(n - 1):]) if len(v.shape) == n else v.view(*batch_dims)) for v in
+                       ret]
+            else:
+                if is_tensor_like(ret):
+                    if len(ret.shape) == n:
+                        ret = ret.view(*batch_dims, *ret.shape[-(n - 1):])
+                    else:
+                        ret = ret.view(*batch_dims)
+            return ret
+
+        return wrapper
+
+    return _handle_batch_input
 
 
 def ensure_tensor(device, dtype, *args):
